bdfc5f74b23c23a5a0982cc71a9254105c0b478c
Jackson Graff <jacksongraff@hotmail.com>
2021-06-11 13:03:19 -0300

    fix: page forwarding

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -24,7 +24,13 @@ export default function SignIn() {
         <Input name="password" type="password" label="Senha" />
       </Stack>
 
-      <Button type="submit" mt="6" colorScheme="pink">Entrar</Button>
+      <Button 
+        as="a" 
+        href="/dashboard"
+        type="submit" 
+        mt="6" 
+        colorScheme="pink"
+      >Entrar</Button>
     </Flex>
     </Flex>
   )
